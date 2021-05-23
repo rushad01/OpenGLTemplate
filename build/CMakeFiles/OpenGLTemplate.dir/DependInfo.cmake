ccfda5fa05fecf5af8373d1ddc9a9aad2ee9bdad
Rushad Tanjim <rushadtanjim3@gmail.com>
2021-05-23 17:32:39 +0600

    Fixed:CMakeLists.txt missing issue

---
+++ build/CMakeFiles/OpenGLTemplate.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Practice/C++/Projects/OpenGLTemplate/src/main.cpp" "CMakeFiles/OpenGLTemplate.dir/src/main.cpp.obj" "gcc" "CMakeFiles/OpenGLTemplate.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/Practice/C++/Projects/OpenGLTemplate/build/externals/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "D:/Practice/C++/Projects/OpenGLTemplate/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  "D:/Practice/C++/Projects/OpenGLTemplate/build/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
